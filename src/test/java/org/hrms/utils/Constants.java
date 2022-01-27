53b06d5d44319d2189fd3fe3d0edd9c51bba09cd
Adam <adamapolat@gmail.com>
2022-01-26 21:00:45 -0500

    Framework initial setup

---
+++ src/test/java/org/hrms/utils/Constants.java
@@ -0,0 +1,9 @@
+package org.hrms.utils;
+
+public class Constants {
+
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "src/test/resources/configs/configuration.properties";
+    public static final String TESTDATA_FILEPATH = "";
+    public static final String SCREENSHOT_FILEPATH = "";
+    public static final int IMPLICIT_WAIT = 10;
+}
